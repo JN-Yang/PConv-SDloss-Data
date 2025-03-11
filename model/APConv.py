3efbc987a28d58bd0ddf30ace5bec53f1f0de78e
JN Yang <115464361+JN-Yang@users.noreply.github.com>
2025-03-11 11:13:53 +0800

    Update APConv.py

--- model/APConv.py
+++ model/APConv.py
@@ -70,6 +70,18 @@ class APC2f(nn.Module):
         else:
             self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
 
+    def forward(self, x):
+        """Forward pass through APC2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
 
 class APBottleneck(nn.Module):
     """Asymmetric Padding bottleneck."""
@@ -83,11 +95,9 @@ class APBottleneck(nn.Module):
         p = [(2,0,2,0),(0,2,0,2),(0,2,2,0),(2,0,0,2)]
         self.pad = [nn.ZeroPad2d(padding=(p[g])) for g in range(4)]
         self.cv1 = Conv(c1, c_ // 4, k[0], 1, p=0)
-        # self.cv1 = nn.ModuleList([nn.Conv2d(c1, c_, k[0], stride=1, padding= p[g], bias=False) for g in range(4)])
         self.cv2 = Conv(c_, c2, k[1], 1, g=g)
         self.add = shortcut and c1 == c2
 
     def forward(self, x):
         """'forward()' applies the YOLO FPN to input data."""
-        # y = self.pad[g](x) for g in range(4)
         return x + self.cv2((torch.cat([self.cv1(self.pad[g](x)) for g in range(4)], 1))) if self.add else self.cv2((torch.cat([self.cv1(self.pad[g](x)) for g in range(4)], 1)))
